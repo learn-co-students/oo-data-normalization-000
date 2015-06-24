a0f473d5719b368104135aa75f8bb57fb5cc5881
plai217 <philiplai217@gmail.com>
2015-06-24 14:41:32 -0400

    not sure this one is correct

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,33 @@
 class Song
+
+require 'tempfile'
+
+attr_reader :title, :artist
+
   # code goes here
+  def initialize
+
+  end
+
+  def title=(song)
+    @title = song
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+  def serialize
+    filename = @title.downcase.gsub(/\s/,"_")
+    file = Tempfile.new([filename,'.txt'],'./tmp') 
+    file.write("#{artist.name} - #{title}")
+    file.close
+  end
+
+
 end
+
+song = Song.new
+song.title = "Night Moves"
+song.artist = Artist.new("Bob Seger")
+song.serialize
