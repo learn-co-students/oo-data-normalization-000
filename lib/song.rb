a13560392e798ec65f82ce455753670f17286bfa
aeremin <aeremin@maxus.ru>
2015-07-02 18:07:32 +0300

    correction

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,14 @@
 require 'tempfile'
+require 'pry'
 class Song
   # code goes here
   attr_accessor :title, :artist
   def serialize
     filename = @title.downcase.gsub(/\s/,'_')
-	file = Tempfile.new(filename, '.txt')
+	file = Tempfile.new([filename, '.txt'])
+	binding.pry
 	file.write("#{@artist.name} - #{@title}")
+	binding.pry
 	file.close
   end
 end
