2eae19b27e9e40247c85119409e769aee3d84a6f
aeremin <aeremin@maxus.ru>
2015-07-02 18:00:45 +0300

    let's try

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,11 @@
+require 'tempfile'
 class Song
   # code goes here
+  attr_accessor :title, :artist
+  def serialize
+    filename = @title.downcase.gsub(/\s/,'_')
+	file = Tempfile.new(filename, '.txt')
+	file.write("#{@artist.name} - #{@title}")
+	file.close
+  end
 end
