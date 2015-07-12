85e152ba7133cdfbc91e46b09e7ae09bdff8a0c2
mbrickenworthe <adamwinters456@gmail.com>
2015-07-11 21:19:59 -0400

    New one for remote build

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
 
   def serialize
     song = title.sub(/\s/, "_")
-    t=Tempfile.new("#{song.downcase}", "/Users/awinters/Labs/oo-data-normalization-000/tmp",)
+    t=Tempfile.new("#{song.downcase}", ".txt",)
     t << "#{artist.name} - #{title}"
     t.close
   end
