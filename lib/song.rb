7dc9cadd4802dfbdf3ead0a3bfedc716889251a5
bidah <bartoindahouse@gmail.com>
2015-08-06 06:11:24 -0400

    done

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,23 @@
 class Song
-  # code goes here
+	attr_accessor :title, :artist
+
+	def artist=(artist)
+		@artist = artist
+	end
+
+	def serialize
+		file = Tempfile.new(["#{@title.downcase.gsub(' ','_')}", ".txt"], "tmp")
+		file.write("#{@artist.name} - #{self.title}")
+		file.close
+	end
 end
+
+class Artist
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
+	end
+end
+
+
