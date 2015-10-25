c2bc6a526962fbbfb0b49ea9c385a6e76986a5a4
NoneOfMaster <rcraigt@gmail.com>
2015-10-24 21:19:54 -0700

    funny tempfile pun

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,14 @@
+require 'pry'
+
 class Song
-  # code goes here
+
+  attr_accessor :artist, :title
+
+
+  def slugify
+    song = Tempfile.new(["#{self.title.downcase.gsub(" ","_")}", ".txt"],"tmp")
+    song.write("#{self.artist.name} - #{self.title}")
+    song.close
+  end
+
 end
