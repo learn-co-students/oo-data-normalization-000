c906abfbe1752a9d56b21196936100baa60b6821
OnlyHughMan <hughvidler@hotmail.com>
2017-05-24 02:18:04 +1000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,13 @@
 class Song
-  # code goes here
+  	# code goes here
+  	attr_accessor :artist, :title
+
+  	def slugify
+  	   file = Tempfile.new([@title.downcase.gsub(" ", "_"), ".txt"], "tmp")
+    	file.write("#{@artist.name} - #{@title}")
+   	file.read
+		file.close 
+	end
+
+
 end
